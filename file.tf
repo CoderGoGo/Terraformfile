618bd2c8e6334cf0cf3c133df77efe77305c6417
CoderGoGo <49941086+CoderGoGo@users.noreply.github.com>
2020-02-19 14:41:20 +0200

    Update file.tf

--- file.tf
+++ file.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "instance" {
   instance_type           = "t3.micro"
   ami                     = "ami-0f1d8c8ad70ce9c62"
   key_name                = "WindowsServer16WithConteiners"
-  vpc_security_group_ids  = [aws_security_group.webserver.id]
+  vpc_security_group_ids  = [aws_security_group.web.id]
   user_data = <<EOF
 #!/bin/bash
 sudo -s
@@ -29,9 +29,9 @@ tags = {
 
 } 
 
-resource "aws_security_group" "webserver" {
-  name        = "Webserver security_group"
-  description = "Webserver security_group"
+resource "aws_security_group" "web" {
+  name        = "Web security_group"
+  description = "Web security_group"
 
   ingress {
     
