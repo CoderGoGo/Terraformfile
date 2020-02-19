3e2b5e167a88a1ef2909f11bf20940cf7d309b3f
root <root@ip-172-31-31-199.eu-north-1.compute.internal>
2020-02-19 11:51:54 +0000

    1.0

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+node('master') {
+    
+    stage('terraform init') {
+ 
+          sh 'terraform init ../terraform'
+          sh 'terraform apply -auto-approve ../terraform'
+          sh 'terraform output > ip'
+	  sh 'cat ip | cut -d \' \' -f 3 > ip_instance'
+	  
+    }
+    }
---
+++ file.tf
@@ -0,0 +1,70 @@
+#----------------------------------------------------------------
+#                                                               -
+#                        WebServer                              -
+#                                                               -
+#----------------------------------------------------------------
+
+provider "aws" {}
+
+resource "aws_instance" "instance" {
+  instance_type           = "t3.micro"
+  ami                     = "ami-0f1d8c8ad70ce9c62"
+  key_name                = "WindowsServer16WithConteiners"
+  vpc_security_group_ids  = [aws_security_group.webserver.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo -s
+yum -y update
+yum -y install httpd
+myip=`curl http://ipv4.icanhazip.com`
+echo "<h2>IP: $myip </h2><br> " > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+tags = {
+    Name = "WebServer"
+    Owner = "Denis Scherbakov"
+}
+
+} 
+
+resource "aws_security_group" "webserver" {
+  name        = "Webserver security_group"
+  description = "Webserver security_group"
+
+  ingress {
+    
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+
+  }
+}
+
+output "ip" {
+  value = aws_instance.instance.public_ip 
+}
