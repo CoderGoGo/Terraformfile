e9ed4b52c1bf6ebc5b0a373eabfa1926f9cb68e8
CoderGoGo <49941086+CoderGoGo@users.noreply.github.com>
2020-03-02 12:27:27 +0200

    Update file.tf

--- file.tf
+++ file.tf
@@ -6,7 +6,30 @@
 
 provider "aws" {}
 
-resource "aws_instance" "instance" {
+resource "aws_instance" "WebServer1" {
+  instance_type           = "t3.micro"
+  ami                     = "ami-0f1d8c8ad70ce9c62"
+  key_name                = "WindowsServer16WithConteiners"
+  vpc_security_group_ids  = [aws_security_group.web.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo -s
+yum -y update
+yum -y install httpd
+myip=`curl http://ipv4.icanhazip.com`
+echo "<h2>IP: $myip </h2><br> " > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+tags = {
+    Name = "WebServer"
+    Owner = "Denis Scherbakov"
+}
+
+} 
+
+resource "aws_instance" "WebServer2" {
   instance_type           = "t3.micro"
   ami                     = "ami-0f1d8c8ad70ce9c62"
   key_name                = "WindowsServer16WithConteiners"
@@ -66,5 +89,6 @@ resource "aws_security_group" "web" {
 }
 
 output "ip" {
-  value = aws_instance.instance.public_ip 
+  value = aws_instance.WebServer1.public_ip
+  value = aws_instance.WebServer2.public_ip
 }
