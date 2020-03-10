5ec69223f4e2e3e8ede3318131789dc31696300a
CoderGoGo <49941086+CoderGoGo@users.noreply.github.com>
2020-03-10 22:29:41 +0200

    Update file.tf

--- file.tf
+++ file.tf
@@ -11,7 +11,8 @@ resource "aws_instance" "WebServer" {
   instance_type           = "t3.micro"
   ami                     = "ami-0f1d8c8ad70ce9c62"
   key_name                = "WindowsServer16WithConteiners"
-  private_ip             = "172.31.36.${count.index +10}"
+  private_ip             = "172.31.32.${count.index +10}"
+  subnet_id              = "subnet-b62ea3fa"
   vpc_security_group_ids  = [aws_security_group.web.id]
   user_data = <<EOF
 #!/bin/bash
